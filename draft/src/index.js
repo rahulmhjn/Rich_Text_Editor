631f339ecee6b6bd1813dd9f59e5223057021127
rahulmhjn <rahul.mahajan676@gmail.com>
2020-08-10 00:09:46 +0530

    draftjs setup

---
+++ draft/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Editor, EditorState } from "draft-js";
+import "draft-js/dist/Draft.css";
+
+const MyEditor = () => {
+  const [editorState, setEditorState] = React.useState(() =>
+    EditorState.createEmpty()
+  );
+
+  return <Editor editorState={editorState} onChange={setEditorState} />;
+};
+
+ReactDOM.render(<MyEditor />, document.getElementById("root"));
